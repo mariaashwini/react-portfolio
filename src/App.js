3df0c2aed406bc24658e6ccddde3ede0a3291cb2
mariaashwini <127980742+mariaashwini@users.noreply.github.com>
2025-08-05 12:37:50 +0530

    Update App.js to show home page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./output.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-portfolio">
          <Sidebar />
          <Routes>
 <Route path="/" element={<Home/>}/>
